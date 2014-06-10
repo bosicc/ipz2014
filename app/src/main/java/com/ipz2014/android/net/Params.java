f9f4f511721bec7d88ccedeab0f52a5c35f75d23
Borys Pratsiuk <b.pratsiuk@abm-sg.com>
2014-06-10 12:16:58 +0300

    Change URL to production

--- app/src/main/java/com/ipz2014/android/net/Params.java
+++ app/src/main/java/com/ipz2014/android/net/Params.java
@@ -5,8 +5,8 @@ package com.ipz2014.android.net;
  */
 public class Params {
 
-    private static String BASE_URL = "http://fixmeipz.appspot.com/api/";
-    public static String FEEDBACK_URL = BASE_URL + "feedback";
+    private static String BASE_URL = "http://1-dot-ipz2014api.appspot.com/";
+    public static String FEEDBACK_URL = BASE_URL + "getfeedback";
 
     public static String EMAIL = "email";
     public static String FEEDBACK = "feedback";
