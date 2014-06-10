c89b056ba7a345dffc4f6ccb07bf9c8b21d26ff7
Borys Pratsiuk <b.pratsiuk@abm-sg.com>
2014-06-10 11:56:16 +0300

    Update UI, added gradient to background and editText fields, refactore api call

--- app/src/main/java/com/ipz2014/android/fragments/MainFragment.java
+++ app/src/main/java/com/ipz2014/android/fragments/MainFragment.java
@@ -2,9 +2,15 @@ package com.ipz2014.android.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.ipz2014.android.R;
 import com.ipz2014.android.net.APIFacade;
 
@@ -13,19 +19,49 @@ import com.ipz2014.android.net.APIFacade;
  */
 public class MainFragment extends Fragment {
 
+    private static final String TAG = MainFragment.class.getSimpleName();
+    private EditText editEmail;
+    private EditText editFeedback;
+    private ProgressBar progress;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(com.ipz2014.android.R.layout.fragment_main, container, false);
+        editEmail = (EditText) rootView.findViewById(R.id.editTextEmail);
+        editFeedback = (EditText) rootView.findViewById(R.id.editTextFeedback);
+        progress = (ProgressBar) rootView.findViewById(R.id.progressBar);
+        progress.setVisibility(View.INVISIBLE);
 
         rootView.findViewById(R.id.btnSend).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                APIFacade.getInstance().sendUserFeedback("user1@gmail.com", "Oh! Інженерія програмного забезпечення " +
-                        "2014 is Rocking!!!");
+                showProgress(true);
+                String email = editEmail.getText().toString();
+                String feedback = editFeedback.getText().toString();
+                APIFacade.getInstance().sendUserFeedback(email, feedback, new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String s) {
+                            showProgress(false);
+                            Log.d(TAG, "onResponse() [response=" + s+"]");
+                            Toast toast = Toast.makeText(getActivity(), s, Toast.LENGTH_SHORT);
+                            toast.show();
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError volleyError) {
+                            showProgress(false);
+                            Log.w(TAG, "onError() [volleyError=" + volleyError + "]");
+                        }
+                    });
             }
         });
 
         return rootView;
+    }
+
+    private void showProgress(boolean isShow) {
+        progress.setVisibility(isShow ? View.INVISIBLE : View.INVISIBLE);
     }
 }
--- app/src/main/java/com/ipz2014/android/net/APIFacade.java
+++ app/src/main/java/com/ipz2014/android/net/APIFacade.java
@@ -1,15 +1,23 @@
 package com.ipz2014.android.net;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.ipz2014.android.IpzApp;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by bosicc on 09.06.2014.
  */
 public class APIFacade {
+
+    /**
+     * Default encoding for POST or PUT parameters. S}.
+     */
+    private static final String DEFAULT_PARAMS_ENCODING = "UTF-8";
 
     private static APIFacade instance = null;
 
@@ -20,32 +28,60 @@ public class APIFacade {
         return instance;
     }
 
+    // NOTE: It's just another way how make REST call over GET method
+//    /**
+//     * Send our data to server side
+//     * @param email
+//     * @param feedback
+//     */
+//    public void sendUserFeedback(String email, String feedback){
+//
+//        String uri = String.format(Params.FEEDBACK_URL +
+//                        "?" + Params.EMAIL + "=%1$s&"+Params.FEEDBACK+"=%2$s",  email, feedback);
+//
+//        StringRequest request = new StringRequest(Request.Method.GET,
+//                uri,
+//                new Response.Listener<String>() {
+//                    @Override
+//                    public void onResponse(String s) {
+//
+//                    }
+//                },
+//                new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError volleyError) {
+//
+//                    }
+//                });
+//
+//        IpzApp.getInstance().getQueue().add(request);
+//
+//    }
+
+
     /**
-     * Send our data to server side
+     * Send data over POST request
      * @param email
      * @param feedback
      */
-    public void sendUserFeedback(String email, String feedback){
+    public void sendUserFeedback(final String email, final String feedback, Response.Listener<String> onResponse,
+                                 Response.ErrorListener onError) {
 
-        String uri = String.format(Params.FEEDBACK_URL +
-                        "?" + Params.EMAIL + "=%1$s&"+Params.FEEDBACK+"=%2$s",  email, feedback);
+        RequestQueue queue = IpzApp.getInstance().getQueue();
 
-        StringRequest request = new StringRequest(Request.Method.GET,
-                uri,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String s) {
+        StringRequest request = new StringRequest(Request.Method.POST,
+                Params.FEEDBACK_URL,onResponse, onError
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                Map<String, String> map = new HashMap<String, String>();
+                map.put(Params.EMAIL, email);
+                map.put(Params.FEEDBACK, feedback);
+                return params;
+            }
+        };
 
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError volleyError) {
-
-                    }
-                });
-
-        IpzApp.getInstance().getQueue().add(request);
-
+        queue.add(request);
     }
 }
